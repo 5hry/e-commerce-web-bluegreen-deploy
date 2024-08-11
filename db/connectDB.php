d1c25b3cd076691ca22b8658c5231df98fb00576
im5hry <ngothanhsang000@gmail.com>
2024-08-11 18:32:44 +0700

    update connection to db

--- db/connectDB.php
+++ db/connectDB.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "app-db"; // Tên dịch vụ MySQL trong docker-compose.yml
+$servername = "ec2-13-229-114-31.ap-southeast-1.compute.amazonaws.com"; // EC2 Public DNS
 $username = "user";
 $password = "user";
 $dbname = "is207_database";
+$port = 3306; // MySQL port
 
 // Tạo kết nối
-$connect = new mysqli($servername, $username, $password, $dbname);
-// Kiểm tra kết nối
+$connect = new mysqli($servername, $username, $password, $dbname, $port);
 
+// Kiểm tra kết nối
 if ($connect->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
+    die("Kết nối thất bại: " . $connect->connect_error);
 }
+echo "Kết nối thành công!";
 ?>
