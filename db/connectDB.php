2742aac91adc83790185459c378c033e6ac8347c
im5hry <96819819+5hry@users.noreply.github.com>
2024-09-24 14:22:03 +0700

    Update connectDB.php

--- db/connectDB.php
+++ db/connectDB.php
@@ -1,8 +1,15 @@
 <?php
-$servername = "ec2-54-169-49-30.ap-southeast-1.compute.amazonaws.com"; // EC2 Public DNS
-$username = "user";
-$password = "user";
-$dbname = "is207_database";
+// $servername = "ec2-54-169-49-30.ap-southeast-1.compute.amazonaws.com"; // EC2 Public DNS
+// $username = "user";
+// $password = "user";
+// $dbname = "is207_database";
+
+//WS-2
+$servername = "db.akaisme.click"; // EC2 Public DNS
+$username = "dbakaiusername";
+$password = "akaistark1ngothanhsang";
+$dbname = "MyDBInstance";
+
 $port = 3306; // MySQL port
 
 // Tạo kết nối
