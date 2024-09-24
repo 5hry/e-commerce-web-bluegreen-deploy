262fc52a65083df56e8defa460507ea1a183a4cd
im5hry <96819819+5hry@users.noreply.github.com>
2024-09-24 16:43:11 +0700

    Update connectDB.php

--- db/connectDB.php
+++ db/connectDB.php
@@ -1,14 +1,15 @@
 <?php
-// $servername = "ec2-54-169-49-30.ap-southeast-1.compute.amazonaws.com"; // EC2 Public DNS
-// $username = "user";
-// $password = "user";
-// $dbname = "is207_database";
+//WS-1
+$servername = "ec2-54-169-49-30.ap-southeast-1.compute.amazonaws.com"; // EC2 Public DNS
+$username = "user";
+$password = "user";
+$dbname = "is207_database";
 
 //WS-2
-$servername = "terraform-20240924072244813800000006.ch442gso0btf.ap-southeast-1.rds.amazonaws.com"; // EC2 Public DNS
-$username = "dbakaiusername";
-$password = "akaistark1ngothanhsang";
-$dbname = "MyDBInstance";
+// $servername = "terraform-20240924072244813800000006.ch442gso0btf.ap-southeast-1.rds.amazonaws.com"; // EC2 Public DNS
+// $username = "dbakaiusername";
+// $password = "akaistark1ngothanhsang";
+// $dbname = "MyDBInstance";
 
 $port = 3306; // MySQL port
 
